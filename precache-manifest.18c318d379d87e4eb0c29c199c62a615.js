ea4ce5fe0389d226f5e9188189514c4fb66091f7
Andrew Gilroy <55792206+Andrew-Gilroy@users.noreply.github.com>
2020-02-07 02:15:45 +0100

    Added final build files for github pages deploy

---
+++ precache-manifest.18c318d379d87e4eb0c29c199c62a615.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cc4c89f4d3834e5ddcf2eefe7013e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0641609e3d3ebde7190",
+    "url": "./static/css/main.3a47d39f.chunk.css"
+  },
+  {
+    "revision": "5c5b88c90bbcd4d37639",
+    "url": "./static/js/2.f6285941.chunk.js"
+  },
+  {
+    "revision": "e0641609e3d3ebde7190",
+    "url": "./static/js/main.c786348e.chunk.js"
+  },
+  {
+    "revision": "a83379088d526bf82c3c",
+    "url": "./static/js/runtime-main.4f9c1b6f.js"
+  },
+  {
+    "revision": "b0fcaff9f21337cc10d61b673d40ed6f",
+    "url": "./static/media/logo.b0fcaff9.svg"
+  },
+  {
+    "revision": "c48e480a381d3437ff53ba9dfdc406c7",
+    "url": "./static/media/reactLogo.c48e480a.svg"
+  }
+]);
